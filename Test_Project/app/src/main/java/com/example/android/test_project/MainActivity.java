5037b750f2052b75a978fc730f847a3b4e72551f
Yousef Alkharraz <bo-bader95@hotmail.com>
2022-03-06 23:05:44 +0300

    done

---
+++ Test_Project/app/src/main/java/com/example/android/test_project/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.android.test_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
